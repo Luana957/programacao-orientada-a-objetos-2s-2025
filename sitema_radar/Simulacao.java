cae99ab7438d1cc938ac4843fe84bd3ee02ac9e5
luanaestefany38@gmail.com <luanaestefany38@gmail.com>
2025-09-04 12:38:49 -0300

    simulacao

--- sitema_radar/Simulacao.java
+++ sitema_radar/Simulacao.java
@@ -4,7 +4,7 @@ public class Simulacao {
     public static void main(String[] args) {
         System.out.println("Simulação");
 
-        Carro opala = new Carro("ACLR300","Comodoro", 76,0); //Criando um novo carro
+        Carro opala = new Carro("ACLR300","Comoodoro", 76,0); //Criando um novo carro
           //opala.ano = 76;
       //  opala.modelo ="Comodoro";
        // opala.placa = "ACLR300";
